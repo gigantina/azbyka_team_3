3ebb2ace38edee560318750517488a3ff400ab28
gigantina <57236258+gigantina@users.noreply.github.com>
2020-02-13 18:47:06 +0300

    first deploy

---
+++ main.py
@@ -0,0 +1,14 @@
+import coding
+import decode
+
+
+def main():
+    print('Привет, пользователь! Мы - команда престарелых разработчиков, создавших этот проект.')
+    print('Если хочешь закодировать сообщение, то пиши code, а если наоборот, то decode')
+    message = input()
+    if message == 'code':
+        print('Отлично! Пиши сообщение на латинице:')
+        print(*coding.encode_to_morse(input()))
+    elif message == 'decode':
+        print('Пиши, американский шпион...')
+        print(*decode.decode_from_morse(input()))
